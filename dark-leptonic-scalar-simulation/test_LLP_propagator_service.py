f2f45437f8e1ebc88a4b993fa255fcef35ae53da
Axel Pontén <axel.ponten@physics.uu.se>
2023-11-15 04:08:06 -0600

    manually deleted files

--- dark-leptonic-scalar-simulation/test_LLP_propagator_service.py
+++ dark-leptonic-scalar-simulation/test_LLP_propagator_service.py
@@ -10,7 +10,7 @@ import icecube.dataclasses
 from icecube.dataclasses import *
 import icecube.dataio
 import icecube.phys_services
-from I3Tray import I3Tray, I3Units
+from icecube.icetray import I3Tray, I3Units
 from icecube.simprod.util import BasicCounter
 from icecube.simprod.segments import GenerateCosmicRayMuons, PropagateMuons, GenerateNaturalRateMuons
 from icecube import clsim
@@ -26,21 +26,21 @@ tray = I3Tray()
 
 rand = phys_services.I3GSLRandomService(seed=0)
 
-n_events = 10000
-filename = "MuonGun_DLS_300MeV_bias_0.1_n_"+str(n_events)+"_only_save_LLP.i3"
+n_events = 10
+filename = "Testing_I3PropagatorService_condor.i3"
 
 icetray.set_log_level(icetray.I3LogLevel.LOG_FATAL) # supress warnings from PROPOSAL integration
 
 tray.context['I3RandomService'] = rand
 
 tray.AddModule("I3InfiniteSource", "TheSource",
-               Prefix               = "../GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz",
+               Prefix               = "/data/user/axelpo/LLP-at-IceCube/dark-leptonic-scalar-simulation/resources/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz",
                Stream               = icecube.icetray.I3Frame.DAQ
               )
 
 tray.AddSegment(GenerateNaturalRateMuons, "muongun",
                 NumEvents           = n_events,
-                GCDFile             = "../GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz",
+                GCDFile             = "/data/user/axelpo/LLP-at-IceCube/dark-leptonic-scalar-simulation/resources/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz",
                 mctree_name         = "I3MCTree_preMuonProp",
                 flux_model          = "GaisserH4a_atmod12_SIBYLL"
                )
@@ -50,8 +50,8 @@ tray.AddSegment(PropagateMuonsLLP,
                 RandomService       = rand,
                 InputMCTreeName     = "I3MCTree_preMuonProp",
                 OutputMCTreeName    = "I3MCTree",
-                PROPOSAL_config_SM  = "config_SM.json",
-                PROPOSAL_config_LLP = "config_DLS.json",
+                PROPOSAL_config_SM  = "/data/user/axelpo/LLP-at-IceCube/dark-leptonic-scalar-simulation/resources/config_SM.json",
+                PROPOSAL_config_LLP = "/data/user/axelpo/LLP-at-IceCube/dark-leptonic-scalar-simulation/resources/config_DLS.json",
                 OnlySaveLLPEvents   = True,
                 only_one_LLP        = True,
                )
