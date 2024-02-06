e6659294b31e115b988c2b84a29e527e6701256f
Axel Pontén <axel.ponten@physics.uu.se>
2024-02-06 10:25:17 -0600

    restructured LLPEstimator and squashed bugs

--- sensitivity-estimation/LLPEstimator.py
+++ sensitivity-estimation/LLPEstimator.py
@@ -5,7 +5,7 @@ from collections.abc import Callable
 
 class LLPModel():
     """
-    Class to hold LLP model parameters and production cross section function.
+    Class to hold LLP model parameters and production cross section function used in LLP estimation.
     """
     def __init__(self, name: str, mass: float, eps: float, tau: float, func_tot_xsec: Callable[[float], float]):
         self.name          = name          # such as DarkLeptonicScalar, etc.
@@ -23,7 +23,7 @@ class LLPModel():
         # @TODO: include both hydrogen and oxygen
         return self.func_tot_xsec(energy)
 
-    def get_lifetime(self, gamma = 1.0: float) -> float:
+    def get_lifetime(self, gamma: float = 1.0) -> float:
         """
         Lifetime of the LLP model. Can give gamma to get time dilated lifetime.
         :param gamma: lorentz boost of the LLP. often given by E/m.
@@ -33,22 +33,26 @@ class LLPModel():
 
     def decay_factor(self, l1: float, l2: float, energy: float) -> float:
         """
-        What is probability to decay between l1 and l2 for an LLP with some energy? Integrate an exponential pdf between l1 and l2.
-        :param l1: minimum length before decay. should be same as minimum detectable gap length.
-        :param l2: maximum length before decay.
-        :param energy: energy of the LLP.
-        :return float: between 0-1. the fraction of the decay pdf within length l1 and l2.
+        What is probability to decay between lengths l1 and l2? Integrate exponential pdf between l1 and l2.
+        :param l1: Minimum length before decay in cm. Should be same as minimum detectable gap length.
+        :param l2: Maximum length before decay in cm.
+        :param energy: Energy of the LLP.
+        :return float: Between 0-1. Fraction of the decay pdf within length l1 and l2.
         """
-        if l1 >= l2:
-            # if production point is closer to boundary than minimum gap length, decay factor is zero
-            return 0.0
-        c_gamma_tau = 299792458.0 * get_lifetime(energy/self.mass) # c * gamma * tau
-        prob = np.exp(-l1/c_gamma_tau) - np.exp(-l2/c_gamma_tau) # exp(-l/c*gamma*tau)|^l1_l2
-        if prob < 0 or prob > 1:
-            raise ValueError("LLP decay probability not bounded between 0 and 1.")
+        c_gamma_tau = 29979245800.0 * self.get_lifetime(energy/self.mass) # c [cm/s] * gamma * tau
+        prob = np.exp(-l1/c_gamma_tau) - np.exp(-l2/c_gamma_tau) # \int^l1_l2 1/c*gamma*tau*exp(-l/c*gamma*tau)
+        # check for negative values
+        if type(prob) is np.ndarray:
+            prob[prob < 0] = 0.0 # if l2 is shorter than l1, return 0
+        else:
+            if prob < 0.0:
+                prob = 0.0
         return prob
 
     def print_summary(self):
+        """
+        For testing purposes.
+        """
         print("Parameters of model", self.name, ":")
         print("mass", self.mass)
         print("eps", self.eps)
@@ -61,16 +65,17 @@ class LLPEstimator():
     """
     Class to calculate detectable LLP probability for a list of LLPModels.
     Calculates detectable LLP probability for each model given a muon track (list of ordered length steps and energies).
+    Input expected in meters and GeV. Internally computes with centimeters.
     """
-    def __init__(self, LLPModels: list, min_gap = 50.0: float):
-        self.min_gap = min_gap     # what's the shortest detectable LLP gap, in meters
+    def __init__(self, LLPModels: list, min_gap_meters: float = 50.0):
+        self.min_gap = min_gap_meters*100.0     # what's the shortest detectable LLP gap, in meters. convert to cm
         self.LLPModels = LLPModels # make sure this stay ordered
-        self.LLP_xsec_funcs = np.array([m.calc_tot_xsec for m in self.LLPModels])
+        self.LLP_funcs = [(m.calc_tot_xsec, m.decay_factor) for m in self.LLPModels]
         
     def calc_LLP_probability(self, length_list: list, energy_list: list) -> list:
         """
         Computes the total detectable LLP probability for a muon track for all models in the LLPEstimator. Convolution of segmented thick target approximation and decay factor.
-        :param length_list: List of lengths from 0 (entering detector) to end of detector in meters. Last element should be total length. Should already be trimmed for entry and exit margins.
+        :param length_list: List of lengths from 0 to end of detector in meters, already trimmed for entry/exit margins. Last element should be total length.
         :param energy_list: List of energies of the muon from detector entry to exit in GeV. Ordered with length_list.
         :return list: Returns a list of detectable LLP probabilities. Ordered with list of LLPModels.
         """
@@ -81,58 +86,29 @@ class LLPEstimator():
             raise ValueError("Negative energies not allowed. If muon stopped, give a length and energy list that stops at stopping point.")
             return None
 
-        # @TODO: come up with a more clever way to use numpy functionality to compute the probaiblities for all models as fast as possible
-
+        # @TODO: come up with clever way using numpy functionality to compute the probaiblities faster
+        
         # numpify input
-        length_array = np.asarray(length_list)*100.0 # convert meter to cm
+        length_array = np.asarray(length_list)*100.0 # convert to cm
         energy_array = np.asarray(energy_list)       # GeV
-        n_steps = len(length_array)
+        track_length = length_array[-1]              # length entry to exit of detector (already trimmed for exit and entry margins) in cm
 
-        # parameters for calculations
-        track_length = length_list[-1]                                                           # length from entry to exit of detector (already trimmed for exit and entry margins) in cm
-        l2_array = np.asarray([track_length - l - self.min_gap for l in length_array])           # from production point to furthest available decay point
-        delta_L = np.asarray([length_array[i+1] - length_array[i] for i in range(n_steps) - 1)]) # step length, in cm
-        n_oxygen = 6.02214076e23 * 0.92 / 18                                                     # number density of oxygen in ice
-        n_hydrogen = 2*n_oxygen                                                                  # number density of hydrogen in ice
-
-        # calculate decay factors and total cross sections
-        matrix_decay_factors = np.row_stack([decay_factor(self.min_gap, l2_array, energy_array)])     # 2D matrix with rows corresponding to each model
-        matrix_tot_xsec = np.col_stack([n_oxygen*func(energy_array) for func in self.LLP_xsec_funcs]) # 2D matrix with cols corresponding to each model
-
-        # compute probability = sum( delta_L * decay_factor * tot_xsec * number_density )
-        probabilities = matrix_decay_factors @ matrix_tot_xsec @ delta_L # NxM*MxN*Nx1 where N is # of models and M is # of length steps
+        # parameters for calculations                         
+        l2_array = track_length - length_array - self.min_gap # from production point to furthest available decay point
+        delta_L = np.append(np.diff(length_array), 0)         # step length, in cm
+        n_oxygen = 6.02214076e23 * 0.92 / 18                  # number density of oxygen in ice
+        n_hydrogen = 2*n_oxygen                               # number density of hydrogen in ice
+        
+        # @TODO: also include hydrogen contribution
+        # compute probability = sum( delta_L * decay_factor * number_density * tot_xsec )
+        matrix_for_calc = np.row_stack([decay(self.min_gap, l2_array, energy_array)*n_oxygen*xsec(energy_array)
+                                             for xsec, decay in self.LLP_funcs]) # 2D matrix with rows corresponding to each model
+        probabilities = matrix_for_calc @ delta_L # NxM*Mx1 where N is # of models and M is # of length steps
         return probabilities
 
     def get_LLPModels(self) -> list:
         return self.LLPModels
 
-########## Helper functions ##########
-def calculate_DLS_lifetime(mass, eps):
-    """ lifetime at first order of Dark Leptonic Scalar. two body decay into e+mu """
-    m_e = 0.00051099895000
-    m_mu = 0.1056583755
-    p1 = np.sqrt( (mass**2 - (m_e + m_mu)**2) * (mass**2 - (m_e - m_mu)**2) ) / (2 * mass) # momentum of electron
-    width = eps**2 / (8 * np.pi) * p1 * (1 - (m_e**2 + m_mu**2) / mass**2)
-    GeV_to_s = 6.582e-25
-    return GeV_to_s * 1 / width
 
-def generate_DLSModels(masses, epsilons, names, table_paths):
-    LLPModel_list = []
-    for mass, eps, name, path in zip(masses, epsilons, names, table_paths):
-        # lifetime
-        tau = calculate_DLS_lifetime(mass, eps)
-        # tot_xsec function from interpolation tables
-        df = pd.read_csv(path, names=["E0", "totcs"])
-        func_tot_xsec_unscaled = interp1d(df["E0"], df["totcs"],kind="quadratic")
-        func_tot_xsec          = lambda energy : eps**2 * func_tot_xsec_unscaled(energy)
-        # create new LLPModel
-        LLPModel_list.append(LLPModel(name, mass, eps, tau, func_tot_xsec))
-    return LLPModel_list
-
-def generate_DLS_WW_oxygen_paths(masses):
-    folder = "/data/user/axelpo/LLP-at-IceCube/sensitivity-estimation/cross_section_tables/"
-    paths  = [folder+"totcs_WW_m_{:.3f}.csv".format(m) for m in masses]
-    return paths
-        
         
     
