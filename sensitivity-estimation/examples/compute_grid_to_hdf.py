30ccb2bd97b0a0cf4ab6511c0dbcbee908fadf1b
Axel Pontén <axel.ponten@physics.uu.se>
2024-03-08 06:57:47 -0600

    bug fix nfiles and changed grid size

--- sensitivity-estimation/examples/compute_grid_to_hdf.py
+++ sensitivity-estimation/examples/compute_grid_to_hdf.py
@@ -35,28 +35,52 @@ outputfile = params["outputfile"] + "_" + str(params["nfiles"]) + "_files.hdf5"
 print(outputfile)
 # infiles
 filelist = list(glob.glob("/data/sim/IceCube/2020/generated/CORSIKA-in-ice/20904/0198000-0198999/detector/IC86.2020_corsika.020904.198*.i3.zst"),)
-n_files = 1 # how many files to use?
-filelist = filelist[0:n_files]
+filelist = filelist[0:params["nfiles"]] # how many files to use?
 print("Number of CORSIKA files used:", len(filelist))
 gcdfile = "/data/user/axelpo/LLP-at-IceCube/dark-leptonic-scalar-simulation/resources/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz"
 
 # create LLP models
-masses = [0.107, 0.108, 0.109, 0.110, 0.11, 0.112, 0.113, 0.114, 0.115, 0.116, 0.117, 0.118, 0.119, 0.120, 0.121, 0.122, 0.123, 0.124, 0.125, 0.127, 0.130, 0.134, 0.138, 0.145, 0.15]
-epsilons = np.logspace(-2.5, -6.3, 40)
-combinations = list(product(masses, epsilons)) # create grid
-masses   = [item[0] for item in combinations] # flatten
-epsilons = [item[1] for item in combinations] # flatten
+# @TODO: read in grid points from config file
+# @TODO: fix so large number of grid points doesn't crash the program
+# masses       = [0.107, 0.108, 0.109, 0.110, 0.111, 0.112, 0.113,
+#                 0.114, 0.115, 0.116, 0.117, 0.118, 0.119, 0.120,
+#                 0.121, 0.122, 0.123, 0.124, 0.125, 0.126, 0.127,
+#                 0.128, 0.129, 0.130, 0.131, 0.132, 0.133, 0.134,
+#                 0.135, 0.136, 0.137, 0.138, 0.139, 0.140, 0.142,
+#                 0.145, 0.15]
+# epsilons     = np.logspace(-2.5, -6, 50)
+
+masses       = [0.107, 0.108, 0.109, 0.110, 0.111, 0.112, 0.113,
+                0.114, 0.115, 0.116, 0.117, 0.118, 0.119, 0.120,
+                0.121, 0.122, 0.123, 0.124, 0.125]
+epsilons     = np.logspace(-2.5, -4, 25)
+
+# create grid
+combinations = list(product(masses, epsilons))
+#masses       = [item[0] for item in combinations] # flatten
+#epsilons     = [item[1] for item in combinations] # flatten
+
+# only keep points that might be interesting
+masses = []
+epsilons = []
+for item in combinations:
+    if item[0] > 0.120 and item[1] > 8e-5:
+        continue
+    masses.append(item[0])
+    epsilons.append(item[1])
+
+names        = ["DLS" for _ in masses]
+# utility funcs to create LLPModel objects
+table_paths  = generate_DLS_WW_oxygen_paths(masses, folder = "../cross_section_tables/")
+DLS_models   = generate_DLSModels(masses, epsilons, names, table_paths)
 print(masses, epsilons)
-names    = ["DLS" for _ in masses]
-table_paths = generate_DLS_WW_oxygen_paths(masses, folder = "../cross_section_tables/")
-DLS_models = generate_DLSModels(masses, epsilons, names, table_paths)
 
 # create LLPEstimator
 min_gap = 50.0 # minimum detectable LLP gap
 DLS_estimator = LLPEstimator(DLS_models, min_gap)
 
 # detector parameters
-n_steps = 50
+n_steps = 80
 
 # which frame objects to save to hdf5 file
 keys = ["LLPProbabilities",
