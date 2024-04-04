b4ee820a3dd5119b84aa19bfe206c758d9cff4b0
Axel Pontén <axel.ponten@physics.uu.se>
2024-04-04 07:57:37 -0500

    updated argparse

--- reconstruction/i3-to-parquet-conversion/example_convert.py
+++ reconstruction/i3-to-parquet-conversion/example_convert.py
@@ -6,23 +6,30 @@ if __name__ == "__main__":
     # read in parser
     parser = argparse.ArgumentParser('convert_DLS')
 
-    # gcd
-    gcdfile = "/data/sim/sim-new/downloads/GCD/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz"
+    parser.add_argument("-t", "--target-folder", action="store",
+            type=str, default="/data/user/axelpo/conversion_testing_ground/", dest="target-folder",
+            help="Directory where to store parquet output. Include backslash.")
+    parser.add_argument("-n", "--num-events-per-file", action="store",
+            type=int, default=1000, dest="num-events-per-file",
+            help="Number of events per parquet file.")
+    parser.add_argument("-g", "--gcdfile", action="store",
+            type=str, default="/data/sim/sim-new/downloads/GCD/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz", dest="gcdfile",
+            help="GCD file.")
+    parser.add_argument("-e", "--encoding", action="store",
+        type=str, default="mod_harnisch", dest="encoding",
+        help="Encoding type from feature config to be passed to icecube.ml_suite.EventFeatureFactory")
+    
+    params = vars(parser.parse_args())  # dict()
 
     # which files to convert? list of paths
-    # filenames = ["test.i3.gz"]
     filenames = ["L2test2.i3.gz"]
-    #df_filenames = pd.read_csv("i3files.csv")
-    #filenames = list(df_filenames)
-    # print("filenames", filenames)
-    
-    target_folder = "/data/user/axelpo/conversion_testing_ground/"
     
     # run conversion
     Converter(
             filenames,
-            target_folder,
+            target_folder = params["target-folder"],
             encoding_type="mod_harnisch",
             pulse_series_name="InIcePulses",
-            gcdfile=gcdfile,
-            num_events_per_file=300)
+            gcdfile=params["gcdfile"],
+            num_events_per_file=params["num-events-per-file"]
+    )
