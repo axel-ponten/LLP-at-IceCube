759c1ae64c793bb4c3a744dd5ebe8b9e3f077c63
Axel Ponten <axelpo@icecube.wisc.edu>
2024-05-02 09:34:48 +0200

    added parse args

--- reconstruction/i3-to-parquet-conversion/example_convert.py
+++ reconstruction/i3-to-parquet-conversion/example_convert.py
@@ -7,34 +7,43 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser('convert_DLS')
 
     parser.add_argument("-t", "--target-folder", action="store",
-            type=str, default="/data/user/axelpo/conversion_testing_ground/", dest="target-folder",
+            type=str, required=True, dest="target-folder",
             help="Directory where to store parquet output. Include backslash.")
+    parser.add_argument("-i", "--input-file", action="store",
+            type=str, required=True, dest="input-file",
+            help="Input i3 file.")
     parser.add_argument("-n", "--num-events-per-file", action="store",
             type=int, default=1000, dest="num-events-per-file",
             help="Number of events per parquet file.")
+    parser.add_argument("-r", "--row-group-number", action="store",
+            type=int, default=100, dest="row-group-number",
+            help="Number of events per row group in parquet file.")
     parser.add_argument("-g", "--gcdfile", action="store",
             type=str, default="/data/sim/sim-new/downloads/GCD/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz", dest="gcdfile",
             help="GCD file.")
     parser.add_argument("-e", "--encoding", action="store",
-        type=str, default="mod_harnisch", dest="encoding",
+        type=str, default="llp_test_config", dest="encoding",
         help="Encoding type from feature config to be passed to icecube.ml_suite.EventFeatureFactory")
     parser.add_argument("--no-llp", action="store_false",
         default=True, dest="is-llp",
         help="Is the dataset an LLP MC simulation?")
+    parser.add_argument("-p", "--pulse-series-name", action="store",
+            type=str, default="InIcePulses", dest="pulse-series-name",
+            help="Name of pulse series to use for pulse extraction.")
     
     params = vars(parser.parse_args())  # dict()
 
     # which files to convert? list of paths
-    filenames = ["L2test2.i3.gz"]
+    filenames = [params["input-file"]]
     
     # run conversion
     Converter(
             filenames,
             target_folder = params["target-folder"],
-            encoding_type="mod_harnisch",
-            pulse_series_name="InIcePulses",
+            encoding_type=params["encoding"],
+            pulse_series_name=params["pulse-series-name"],
             gcdfile=params["gcdfile"],
             num_events_per_file=params["num-events-per-file"],
-            num_per_row_group=100,
+            num_per_row_group=params["row-group-number"],
             is_llp=params["is-llp"],
     )
