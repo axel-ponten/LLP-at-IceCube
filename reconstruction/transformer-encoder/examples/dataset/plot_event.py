2e6baaeaf512c75413f3995fb8d86b5490e856a9
Axel Ponten <axelpo@icecube.wisc.edu>
2024-05-07 16:21:02 +0200

    plot events

--- reconstruction/transformer-encoder/examples/dataset/plot_event.py
+++ reconstruction/transformer-encoder/examples/dataset/plot_event.py
@@ -9,35 +9,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-# filepaths
-top_folder = "/home/axel/i3/i3-pq-conversion-files/DLS-115-5e-6/"
-index_file_path = top_folder + "indexfile.pq"
-total_index_info = pd.read_parquet(index_file_path)
-feature_indices_file_path = top_folder + "feature_indices.yaml"
-file_paths = glob.glob(top_folder + "L2*.pq")
 
-# create dataset
-dataset = LLPDataset(
-    index_file_path,
-    file_paths,
-    feature_indices_file_path,
-    normalize_data=False,
-    device="cuda",
-    dtype=torch.float32,
-    shuffle_files=True,
-)
-
-# get event
-for i in range(5):
-    data, label = dataset[i]
-
+def plot_event(data, label):
     # List of 3D positions
     # positions = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
-    hits = data[:, :4].tolist()
+    hits = data.squeeze()[:,:4].cpu().numpy().tolist()
+    print(hits)
     x = [pos[0] for pos in hits]
     y = [pos[1] for pos in hits]
     z = [pos[2] for pos in hits]
-    s = [10**pos[3]-1 for pos in hits]
+    s = [10**pos[3]-1 for pos in hits] # total charge from log(1+charge)
     # label
     label = label.cpu().numpy().tolist()
     prod_x = label[0]
@@ -48,7 +29,7 @@ for i in range(5):
     decay_z = label[5]
 
     # Create a 3D plot
-    fig = plt.figure(figsize=(10, 8))  # Set the figure size to 10x8 inches
+    fig = plt.figure(figsize=(12,12))  # Set the figure size to 10x8 inches
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(x, y, z, s=s)
     ax.scatter(prod_x, prod_y, prod_z, color='red')
@@ -71,19 +52,49 @@ for i in range(5):
     ax.set_zlabel('Z')
     ax.set_title('Red = prod, Green = decay')
 
+    # Set all axes limits
+    xmin = -600
+    xmax = 600
+    ymin = -600
+    ymax = 600
+    zmin = -600
+    zmax = 600
     
-    # Get the current axes limits
-    x_limits = ax.get_xlim()
-    y_limits = ax.get_ylim()
-    z_limits = ax.get_zlim()
-
-    # Find the largest limit
-    largest_limit = max(max(x_limits), max(y_limits), max(z_limits))
-
-    # Set all axes limits to be the largest limit
-    ax.set_xlim([-largest_limit, largest_limit])
-    ax.set_ylim([-largest_limit, largest_limit])
-    ax.set_zlim([-largest_limit, largest_limit])
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+    ax.set_zlim([zmin, zmax])
 
     # Show the plot
     plt.show()
+
+# filepaths
+top_folder = "/home/axel/i3/i3-pq-conversion-files/DLS-115-5e-6/"
+index_file_path = top_folder + "indexfile.pq"
+total_index_info = pd.read_parquet(index_file_path)
+feature_indices_file_path = top_folder + "feature_indices.yaml"
+file_paths = glob.glob(top_folder + "L2*.pq")
+
+# create dataset
+dataset = LLPDataset(
+    index_file_path,
+    file_paths,
+    feature_indices_file_path,
+    normalize_data=False,
+    device="cuda",
+    dtype=torch.float32,
+    shuffle_files=True,
+)
+
+dataloader = DataLoader(
+    dataset,
+    batch_size=8,
+    shuffle=False,
+    collate_fn=llp_collate_fn,
+)
+# get event
+for datavecs, datalens, labels in dataloader:
+    for i in range(len(datavecs)):
+        data = datavecs[i]
+        label = labels[i]
+        plot_event(data, label)
+    break
